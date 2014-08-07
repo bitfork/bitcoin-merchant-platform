9f9b020d817317fefa38836ed892caf9b17f5526
bitfork <bitfork.develop@gmail.com>
2014-08-07 09:18:21 +0400

    show course > 1

--- wob/components/widgets/WobCourse.php
+++ wob/components/widgets/WobCourse.php
@@ -5,7 +5,7 @@ class WobCourse extends WobWidget
 
 	public function run()
 	{
-		$pairs = WobPair::model()->pay()->findAll();
+		$pairs = WobPair::model()->pay()->findAll('course>1');
 		$pair = $pairs[0];
 		unset($pairs[0]);
 		$this->render($this->getViewPathTheme().$this->view, array(
